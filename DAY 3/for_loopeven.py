be431176e83dadedd7541c528ecabc5c3cddc0b3
Delaray1 <komenraymond222@gmail.com>
2024-11-17 20:06:51 +0300

    initial commit

--- DAY 3/for_loopeven.py
+++ DAY 3/for_loopeven.py
@@ -1,6 +1,6 @@
 for number in range(100, 0, -1):  
     if number > 0:
-        print(f"{number} is positive", end=", ")
+        print(f"{number} is positive")
         if number % 2 == 0:
             print("and even.")
         else:
